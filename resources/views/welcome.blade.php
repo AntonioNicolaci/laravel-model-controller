86f7bde336f4198fb922c32ceaa4eba9c8acf45d
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-03 15:48:52 +0200

    add cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,31 @@
         @vite('resources/scss/app.scss')
     </head>
     <body>
+        <div id="ms_divCard">
+            @foreach($movies as $movie)
+                <ul class="ms_card">
+                    <li>
+                        <span class="ms_category">Titolo: </span>
+                        <span class="ms_db">{{$movie->title}}</span>
+                    </li>
+                    <li>
+                        <span class="ms_category">Titolo Originale: </span>
+                        <span class="ms_db">{{$movie->original_title}}</span>
+                    </li>
+                    <li>
+                        <span class="ms_category">Nazione del film: </span>
+                        <span class="ms_db">{{$movie->nationality}}</span>
+                    </li>
+                    <li>
+                        <span class="ms_category">Data di uscita: </span>
+                        <span class="ms_db">{{$movie->date}}</span>
+                    </li>
+                    <li>
+                        <span class="ms_category">Voto: </span>
+                        <span class="ms_db">{{$movie->vote}}</span>
+                    </li>
+                </ul>
+            @endforeach
+        </div>
     </body>
 </html>
